f1a1682b1eb4e3b7044b7096e7b34df3a79d3aa0
momin-ctg <mominriyadh@gmail.com>
2015-10-11 19:57:34 +0600

    add files and folder

---
+++ js/js.js
@@ -0,0 +1,7 @@
+<script charset="utf-8" src="//js.hubspot.com/forms/current.js"></script>
+<script>
+  hbspt.forms.create({
+    portalId: '196250',
+    formId: '63462895-bc50-4e92-a367-466a309052bf'
+  });
+</script>
